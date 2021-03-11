afe9fe91f3a6f94ea3007664664800892f52b32a
Max Brosnahan <maximilianbrosnahan@gmail.com>
2021-03-11 12:22:26 +0100

    Raise error when a version mismatch occurs
    Why: This is the easiest way to not sliently ignore this problem for now. In the future this should be handled better.

--- lib/podium/resource.rb
+++ lib/podium/resource.rb
@@ -3,6 +3,12 @@ require "json"
 
 module Podium
   class Resource
+    class VersionMismatchError < StandardError
+      def initialize(podlet_version, manifest_version)
+        super("expected podlet_version=#{podlet_version} to match manifest_version=#{manifest_version}")
+      end
+    end
+
     attr_reader :uri
 
     def initialize(uri)
@@ -14,10 +20,18 @@ module Podium
     end
 
     def fetch
-      Net::HTTP.get(content_uri)
+      response = Net::HTTP.get_response(content_uri)
+      check_podlet_version!(response["podlet-version"])
+      response.body
     end
 
     private
+
+    def check_podlet_version!(version)
+      return if version == manifest.version
+
+      raise VersionMismatchError.new(version, manifest.version)
+    end
 
     def fetch_manifest
       Manifest.new(JSON.parse(Net::HTTP.get(manifest_uri)))
--- spec/lib/podium/resource_spec.rb
+++ spec/lib/podium/resource_spec.rb
@@ -2,17 +2,28 @@ require "spec_helper"
 
 RSpec.describe Podium::Resource do
   let(:uri) { "http://pizza.localhost" }
+  let(:manifest_str) { File.read("spec/fixtures/header_manifest.json") }
+  let(:manifest_version) { JSON.parse(manifest_str).fetch("version") }
 
   subject { described_class.new(uri) }
 
   describe "#fetch" do
     it "returns the content for the podlet" do
       stub_request(:get, "http://pizza.localhost/manifest.json")
-        .to_return(body: File.read("spec/fixtures/header_manifest.json"))
+        .to_return(body: manifest_str)
       stub_request(:get, "http://pizza.localhost/")
-        .to_return(body: "tomato and cheese")
+        .to_return(body: "tomato and cheese", headers: { "podlet-version" => manifest_version })
 
       expect(subject.fetch).to eql("tomato and cheese")
+    end
+
+    it "raises an error if the podlet-version does not match the manifest version" do
+      stub_request(:get, "http://pizza.localhost/manifest.json")
+        .to_return(body: manifest_str)
+      stub_request(:get, "http://pizza.localhost/")
+        .to_return(body: "tomato and cheese", headers: { "podlet-version" => "not manifest version" })
+
+      expect { subject.fetch }.to raise_error(Podium::Resource::VersionMismatchError)
     end
   end
 end
