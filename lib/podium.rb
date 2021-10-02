b16117f5e89e6ace59a1426b1fb9ed763033de9a
Max Brosnahan <maximilianbrosnahan@gmail.com>
2021-10-02 17:08:45 +0200

    Move controller to app folder
    Why: Otherwise rails needs to be defined.

--- lib/podium.rb
+++ lib/podium.rb
@@ -7,7 +7,6 @@ require_relative "podium/manifest"
 require_relative "podium/configuration"
 require_relative "podium/controller_helpers"
 require_relative "podium/podlet_preview"
-require_relative "podium/podlet_preview_controller"
 require_relative "podium/podlet_helpers"
 require_relative "podium/core_ext"
 
